ce8b5fdcd852135ac006082476c825fee5f084e3
Belén <be23mg@gmail.com>
2019-01-05 17:21:09 +0000

    finished mobile menu

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,8 @@
 (function() {
   'use strict';
   const header = document.querySelector('header');
-  document.getElementById('toggle').addEventListener('click', () => {
+  document.getElementById('toggle').addEventListener('click', (e) => {
+    e.preventDefault();
     header.classList.toggle('expanded');
   });
 }());
